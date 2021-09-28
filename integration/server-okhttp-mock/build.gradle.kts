333cad3b73f27dfaedbe712675b031a172cc87f2
Johannes Müller <johannes.mueller@freiheit.com>
2021-09-28 22:37:07 +0200

    add okhttp mockwebserver binding

---
+++ integration/server-okhttp-mock/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java-library")
+    kotlin("jvm")
+    id("maven-publish")
+}
+
+group = "dev.jomu.krpc"
+version = "0.1.4"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-common"))
+    implementation(project(":runtime"))
+    implementation(libs.kotlin.coroutines.core)
+    
+//    api(libs.okhttp.core)
+//    implementation(libs.okhttp.core)
+//    implementation(libs.okhttp.mockwebserver)
+    implementation(libs.okhttp.mockwebserver)
+
+    api(libs.kotlin.serialization.json)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("server-okhttp-mock") {
+            from(components["java"])
+        }
+    }
+}
---
+++ integration/server-okhttp-mock/src/main/kotlin/dev/jomu/krpc/runtime/Server.kt
@@ -0,0 +1,49 @@
+package dev.jomu.krpc.runtime
+
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.DeserializationStrategy
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
+import okhttp3.Headers
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.RecordedRequest
+
+class KrpcDispatcher(val server: KrpcServer) : Dispatcher() {
+    override fun dispatch(request: RecordedRequest): MockResponse {
+        return runBlocking {
+            val response = server.handleRequest(request.path.orEmpty(), OkHttpCall(request))
+
+            val headers = Headers.Builder().apply {
+                response.metadata.forEach { (key, value) ->
+                    add("krpc-$key", value)
+                }
+            }.build()
+
+            MockResponse().setResponseCode(200).setHeaders(headers).setBody(response.encode(JsonStringEncoder))
+        }
+    }
+}
+
+private object JsonStringEncoder : JsonEncoder<String> {
+    override suspend fun <U> encode(json: Json, serializer: SerializationStrategy<U>, value: U): String {
+        return json.encodeToString(serializer, value)
+    }
+}
+
+private class OkHttpCall(val request: RecordedRequest) : Call {
+    @OptIn(ExperimentalSerializationApi::class)
+    override suspend fun <T> readRequest(json: Json, deserializer: DeserializationStrategy<T>): T {
+        return json.decodeFromStream(deserializer, request.body.inputStream())
+    }
+
+    override val metadata: Metadata
+        get() = request.headers.toMetadata()
+
+}
+
+fun Headers.toMetadata(): Metadata = Metadata(toMap()
+    .filterKeys { it.startsWith("krpc-") }
+    .mapKeys { (value, _) -> value.removePrefix("krpc-") })
